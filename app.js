01abb76c4a243b5f0eb449e696db350e545542bf
senjansen <senjansen.sj@gmail.com>
2020-01-27 12:20:10 +0700

    change port to 80

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 80 || process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
